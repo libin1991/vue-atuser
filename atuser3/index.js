a93ed91dc3ee45d3b78af179e95e6ad319b0ab73
libin16 <libin16@bj-m-208311a.local>
2018-12-24 19:33:41 +0800

    l

---
+++ atuser3/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './index.vue'
+import '@/assets/css/reset.less'
+
+new Vue({
+    render: h => h(App)
+  }).$mount('#app')
---
+++ atuser3/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="atuser">
+		<at :members="members" @enterSend="send" v-model="inputcontent">
+			<textarea class="editor"></textarea>
+		</at>
+	</div>
+</template>
+
+<script>
+	import at from './atuser.vue'
+	export default {
+		data() {
+			return {
+				members: [123, 12, 1234, 12345, "小花", "小花华", "小三"],
+				inputcontent: "" //用户输入内容初始值
+
+			};
+		},
+		components: {
+			at,
+		},
+		methods: {
+			send(e) { //回车发送
+				console.log(e)
+			}
+		}
+	}
+</script>
+
+<style scoped="scoped" lang="less">
+	.atuser {
+		width: 700px;
+		height: 160px;
+		border: 1px solid red;
+		.editor{
+			width: 700px;
+			height: 160px;
+			overflow: hidden;
+			border: 0px;
+			outline: none;
+			resize: none;
+	       -webkit-appearance: none;
+		}
+	}
+</style>
